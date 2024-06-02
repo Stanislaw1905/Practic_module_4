1b5b4663ea6e5fe51fe70afeed68379e766f6930
Stanislaw1905 <stanislaw.chursin@mail.ru>
2024-06-02 21:46:15 +0200

    ////

---
+++ Practic_module_4.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic_module_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сложение двух матриц");
+
+            int[,] matrix1 = new int[5, 5];
+            int[,] matrix2 = new int[5, 5];
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix1[i, j] = rand.Next(10);
+                    Console.Write($"{matrix1[i, j]} ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+            for (int i = 0;i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix2[i, j] = rand.Next(10);
+                    Console.Write($"{matrix2[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сумма двух матриц ");
+            int[,] matrix3 = new int[5, 5];
+            for (int i = 0; ; i++)
+            {
+                for(int j = 0;j < 5; j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+                    Console.Write($"{matrix3[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Practic_module_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic_module_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк в будующей матрице: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в будующей матрице: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[row, col];
+
+            Random r = new Random();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = r.Next(10);
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
