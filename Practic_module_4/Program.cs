7b1cb00d9f6acf9d0d32007322546bd9064a294e
Stanislaw1905 <stanislaw.chursin@mail.ru>
2024-06-04 20:29:26 +0200

    correction

--- Practic_module_4/Program.cs
+++ Practic_module_4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,18 +19,19 @@ namespace Practic_module_4
             int[,] matrix = new int[row, col];
 
             Random r = new Random();
-
+            int sum = 0; 
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < col; j++)
                 {
                     matrix[i, j] = r.Next(10);
+                    sum += matrix[i, j];
                     Console.Write($"{matrix[i, j]} ");
                 }
                 Console.WriteLine();
-
             }
             Console.WriteLine();
+            Console.WriteLine($"Сумма всех элементов матрицы равна: {sum}");
         }
     }
 }
